4a06a0984f956930182bd9652b60628c06e7ae6e
Aditya Srivastava <adityashri277@gmail.com>
2025-08-23 13:09:59 +0530

    Update index.js
    Fixed the Email Sending Issues

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
- const sendbtn = document.getElementById('sendbtn');
-      const scriptURL = "https://script.google.com/macros/s/AKfycbzDMYS21hJfudpGPdoEofKiFAWnjNMusKCyONpjChBszHLy2obR2LDRkVtM7bvyjbdJMg/exec";
-      const form = document.forms["submit-to-google-sheet"];
+const sendbtn = document.getElementById("sendbtn");
 
-      form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        fetch(scriptURL, { method: "POST", body: new FormData(form) })
-          .then((response) => {
-            setTimeout(() => {
-            }, 2000);
-            form.reset();
-          })
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzDMYS21hJfudpGPdoEofKiFAWnjNMusKCyONpjChBszHLy2obR2LDRkVtM7bvyjbdJMg/exec";
+const form = document.forms["submit-to-google-sheet"];
+const emailInput = form.querySelector("input[name='UserEmail']"); 
+  const emailValue = emailInput.value.trim(); 
 
-          .catch((error) => console.error("Error!", error.message));
-      });
-      sendbtn.addEventListener("click", (e) => {
-        e.preventDefault();
-        fetch(scriptURL, { method: "POST", body: new FormData(form) })
-          .then((response) => {
-            setTimeout(() => {
-            }, 2000);
-            form.reset();
-          })
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  
+  sendbtn.disabled = true;
+  sendbtn.style.cursor= "not-allowed";
+  sendbtn.innerText = "Sending";
 
-          .catch((error) => console.error("Error!", error.message));
-      });
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      setTimeout(() => {
+        sendbtn.disabled = false;
+        sendbtn.style.cursor= "pointer";
+        sendbtn.innerText = "Submit";
+         form.reset();
+      }, 2000);
+
+     
+    })
+
+    .catch((error) => console.error("Error!", error.message));
+});
+sendbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+
+  sendbtn.disabled = true;
+  sendbtn.innerText = "Sending";
+  sendbtn.style.cursor= "not-allowed";
+  
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      setTimeout(() => {
+        sendbtn.disabled = false;
+         sendbtn.style.cursor= "pointer";
+        sendbtn.innerText = "Submit";
+        form.reset();
+      }, 2000);
+      
+    })
+
+    .catch((error) => console.error("Error!", error.message));
+});
+
+
 
